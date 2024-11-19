ca2b7c22ef1310d211eac3d254756caad81a8877
AnamXI <anamggss1227@gmail.com>
2024-11-19 17:08:06 +0800

    V2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        {/* <div id="pdiv" style={{backgroundImage: `url("assets/homebg2.png")`, backgroundSize: "cover"}} >
+        <div style={{height: "1080px"}}></div>
+        </div> */}
+
+        {/*PANEL 1*/}
+        <div class="container">
+        <img src="assets/homebg.png" style={{width: "100%"}}/>
+        </div>
+
+        {/*PANEL 2*/}
+        <div class="container">
+          <img src="assets/goldbg.png" style={{width: "100%"}}></img>
+          <div style={{position: "absolute", left: "45%", top: "45%", color: "black"}} ><h1>HIA</h1></div>
+        </div>
+
+
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        
       </header>
     </div>
   );
