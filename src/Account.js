aca8d1f44eaed842dd423828ca6ff9047951780f
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-11-21 09:29:38 +0800

    V11

--- src/Account.js
+++ src/Account.js
@@ -3,8 +3,9 @@ import Navbar from './Navbar'
 
 export default function Account() {
   return (
-    <div className='App-header'>
+    <div className='App-header' style={{backgroundImage: `url("assets/mainbr.png")`}}>
         <Navbar />
+        <br></br>
         <h1>ACCOUNT</h1>
     </div>
   )
