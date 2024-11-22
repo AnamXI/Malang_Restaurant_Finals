3831d6bfa31282dd789e752fce35526c6d4006a6
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-11-22 12:22:18 +0800

    Food Cards Final

--- src/components/IndividualProduct.js
+++ src/components/IndividualProduct.js
@@ -6,6 +6,13 @@ const IndividualProduct = ({individualProduct}) => {
     return (
     <div className='fcard'>
       <img className='fcardimg'src={individualProduct.ProductImg} alt='Food Showcase'/>
+     
+     <div style={{height:"80px"}}>
+      <h4 style={{fontFamily: "KP, Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif", fontSize: "clamp(24px, 2vw, 30px)"}}>{individualProduct.ProductName}</h4>
+      <p style={{fontSize: "clamp(20px, 2vw, 20px)"}}>{individualProduct.ProductDesc}</p>
+      </div>
+      
+      <button><h3>${individualProduct.ProductPrice}.00</h3></button>
     </div>
   )
 }
