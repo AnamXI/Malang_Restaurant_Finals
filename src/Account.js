ef6e50b59d355a96de202cf9d42cbe3cad3f2353
AnamXI <anamggss1227@gmail.com>
2024-11-23 07:09:08 +0800

    Garen Bautista

--- src/Account.js
+++ src/Account.js
@@ -5,8 +5,11 @@ export const Account = ({ user }) => {
   return (
     <div className='App-header' style={{backgroundImage: `url("assets/mainbr.png")`}}>
         <Navbar user={user}/>
-        <br></br>
-        <h1>ACCOUNT</h1>
+        <div style={{flex: '1', display:'flex', flexDirection:'column',justifyContent:'center', alignItems:'center'}}>
+        <div><img src='assets/AccountPlaceholder.jpg' style={{borderRadius:'200px', }}/></div><br/>
+        <div style={{backgroundColor: '#ffde59', color: 'black', boxShadow:'10px 10px black', borderRadius:'20px'}}>
+          <h1 style={{margin:'2*px'}}>{user}</h1></div>
+        </div>
     </div>
   )
 }
